1ae6e2f4470d489e493330ad8a4349908a747847
Alex Kiura <kiuraalex@gmail.com>
2021-06-03 06:24:43 +0300

    Rename project settings directory

---
+++ config/settings/test.py
@@ -0,0 +1,11 @@
+from .base import *
+
+PROJECT_DIR = os.path.abspath(os.path.dirname(__file__))
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(PROJECT_DIR, 'database_test'),
+        'USER': 'user_tester'
+    }
+}
--- config/urls.py
+++ config/urls.py
@@ -1,4 +1,4 @@
-"""mini_crowdbotics_27676 URL Configuration
+"""config URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.2/topics/http/urls/
