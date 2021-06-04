82a433e35a468f1be38160562a441ed146ca002f
Alex Kiura <kiuraalex@gmail.com>
2021-06-04 17:10:49 +0300

    Add View to Create List Retrieve Update Subscription

--- core/api/v1/views.py
+++ core/api/v1/views.py
@@ -3,8 +3,9 @@ from rest_framework.generics import ListCreateAPIView, \
     RetrieveUpdateDestroyAPIView, ListAPIView, RetrieveAPIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
-from core.models import App, Plan
-from .serializers import AppSerializer, PlanSerializer
+from core.models import App, Plan, Subscription, AppTypeChoices,\
+    AppFrameWorkChoices
+from .serializers import AppSerializer, PlanSerializer, SubscriptionSerializer
 
 
 class AppListCreateAPIView(ListCreateAPIView):
@@ -16,10 +17,15 @@ class AppListCreateAPIView(ListCreateAPIView):
         name = request.data.get("name")
         description = request.data.get("description")
         type_ = request.data.get("type")
-        framework = request.data.get("framework")
         domain_name = request.data.get("domain_name")
         user = request.user
+        framework = request.data.get("framework")
         try:
+            if type_ not in AppTypeChoices:
+                raise ValueError("Type must be one of: Web, Mobile")
+            if framework not in AppFrameWorkChoices:
+                raise ValueError("Framework must be one of: "
+                                 "Django, React Native")
             app = App.objects.create(
                 name=name,
                 description=description,
@@ -54,3 +60,14 @@ class PlanRetrieveAPIView(RetrieveAPIView):
     permission_classes = (IsAuthenticated,)
     serializer_class = PlanSerializer
 
+
+class SubscriptionRetrieveUpdateDestroyAPIView(RetrieveUpdateDestroyAPIView):
+    queryset = Subscription.objects.all()
+    permission_classes = (IsAuthenticated,)
+    serializer_class = SubscriptionSerializer
+
+
+class SubscriptionListCreateAPIView(ListCreateAPIView):
+    queryset = Subscription.objects.all()
+    permission_classes = (IsAuthenticated,)
+    serializer_class = SubscriptionSerializer
