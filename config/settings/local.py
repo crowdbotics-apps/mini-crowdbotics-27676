5b7cdabfd9d80816087c924b9ba4980a0c4b56df
Alex Kiura <kiuraalex@gmail.com>
2021-06-04 09:13:55 +0300

    Configure local and test databases

--- config/settings/local.py
+++ config/settings/local.py
@@ -1,1 +1,10 @@
 from .base import *
+PROJECT_DIR = os.path.abspath(os.path.dirname(__file__))
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(PROJECT_DIR, 'local_db.sqlite3'),
+        'USER': 'user_local'
+    }
+}
